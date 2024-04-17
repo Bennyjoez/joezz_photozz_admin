36279274000a6caa0521a58f7f7e58846cf4df2b
Benson Njuguna <njugunab655@gmail.com>
2024-04-17 12:07:27 +0300

    Add default routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,34 @@ import App from "./App.tsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Error from "./components/error/Error.tsx";
+import Profile from "./pages/Profile.tsx";
+import Bookings from "./pages/Bookings.tsx";
+import Messages from "./pages/Messages.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <Error />,
-    children: [],
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />
+      },
+      {
+        path: "/profile",
+        element: <Profile />
+      },
+      {
+        path: "/bookings",
+        element: <Bookings />
+      },
+      {
+        path: "/messages",
+        element: <Messages />
+      },
+    ],
   },
 ]);
 
