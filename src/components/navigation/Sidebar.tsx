c81c01f4a85ce498bf219667ab709ba6ef6d8936
Benson Njuguna <njugunab655@gmail.com>
2024-04-18 18:31:11 +0300

    Adjust to a minimalistic sidebar

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -3,30 +3,66 @@ import logo from "/logo.png";
 import { IoIosHome } from "react-icons/io";
 import { AiFillProfile, AiFillMessage } from "react-icons/ai";
 import { FaCalendarCheck } from "react-icons/fa";
+import { TfiControlForward, TfiControlBackward } from "react-icons/tfi";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [sideOpen, setSidebarOpen] = useState(false);
   return (
-    <section className="w-[200px] bg-white shadow-sidebar flex flex-col align-center gap z-10" >
-      <div className="grid place-items-center py-4">
-        <img src={logo} alt="Logo" className="h-20" />
+    <section
+      className={`${
+        sideOpen ? "w-[200px] duration-300 ease-in-out" : "w-fit duration-300 ease-in-out"
+      } bg-white shadow-sidebar flex flex-col align-center gap z-10 font-madimi`}
+    >
+      <div className="grid place-items-center p-2 h-[80px]">
+        <img src={logo} alt="Logo" className="h-10" />
       </div>
-      <div className="flex flex-col">
-        <NavLink className="navlink" to="/">
+      <div className={`${!sideOpen ? "text-8rem" : ""} flex flex-col relative`}>
+        <NavLink
+          className={`${sideOpen ? "" : "icons"} navlink`}
+          to="/"
+          title={sideOpen ? "" : "Dashboard"}
+        >
           <IoIosHome />
-          <span>Dashboard</span>
+          {sideOpen && <span>Dashboard</span>}
         </NavLink>
-        <NavLink className="navlink" to="/profile">
+        <NavLink
+          className={`${sideOpen ? "" : "icons"} navlink`}
+          to="/profile"
+          title={sideOpen ? "" : "Profile"}
+        >
           <AiFillProfile />
-          <span>Profile</span>
+          {sideOpen && <span>Profile</span>}
         </NavLink>
-        <NavLink className="navlink" to="/bookings">
+        <NavLink
+          className={`${sideOpen ? "" : "icons"} navlink`}
+          to="/bookings"
+          title={sideOpen ? "" : "Bookings"}
+        >
           <FaCalendarCheck />
-          <span>Bookings</span>
+          {sideOpen && <span>Bookings</span>}
         </NavLink>
-        <NavLink className="navlink" to="/messages">
+        <NavLink
+          className={`${sideOpen ? "" : "icons"} navlink`}
+          to="/messages"
+          title={sideOpen ? "" : "Messages"}
+        >
           <AiFillMessage />
-          <span>Messages</span>
+          {sideOpen && <span>Messages</span>}
         </NavLink>
+        <div className="text-xl text-primary-text font-bold cursor-pointer animate-pulse w-full grid place-content-end">
+          {sideOpen ? (
+            <TfiControlBackward
+              title="Collapse"
+              onClick={() => setSidebarOpen(!sideOpen)}
+            />
+          ) : (
+            <TfiControlForward
+              title="Expand"
+              onClick={() => setSidebarOpen(!sideOpen)}
+            />
+          )}
+        </div>
       </div>
     </section>
   );
