eb419b562106bd1b63aed2d050108db11018e1a0
Benson Njuguna <njugunab655@gmail.com>
2024-04-19 13:16:42 +0300

    Add custom styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,15 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "card": "linear-gradient(#4C49ED, #0A06F4)",
+        "card-lower": "linear-gradient(#FFFFFF50, #FFFFFF00)",
+      },
       colors: {
         'accent': "rgba(76, 73, 237, 1)",
         "primary-text": "#343C6A",
         "secondary-text": "#B1B1B1",
+        "text-white": "",
         "primary-bg": "#F5F7FA",
       },
       boxShadow: {
