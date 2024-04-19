f2dcafa752286417be179cb767c4514193afbaf0
Benson Njuguna <njugunab655@gmail.com>
2024-04-19 13:15:15 +0300

    Add mock data

---
+++ src/data/bookingsData.ts
@@ -0,0 +1,25 @@
+const bookings = [
+  {
+    client: "John Doe",
+    event: "Wedding",
+    reservationDate: "29th Dec 2023",
+    shootLocation: "Nakuru",
+    message: "We may need lighting, reflectors and photoboxes"
+  },
+  {
+    client: "Alice Smith",
+    event: "Pre wedding shoot",
+    reservationDate: "5th Feb 2024",
+    shootLocation: "Nairobi",
+    message: "Outdoor shoot"
+  },
+  {
+    client: "Bob Johnson",
+    event: "Portrait Shots",
+    reservationDate: "10th Mar 2024",
+    shootLocation: "Mombasa",
+    message: "Indoor shoot"
+  }
+];
+
+export default bookings;
