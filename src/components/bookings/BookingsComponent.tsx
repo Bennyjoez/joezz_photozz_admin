cf23c392e2bc05516bacb41bdcb1bbbbb50e121c
Benson Njuguna <njugunab655@gmail.com>
2024-04-22 18:25:47 +0300

    Add link to all bookings

--- src/components/bookings/BookingsComponent.tsx
+++ src/components/bookings/BookingsComponent.tsx
@@ -1,12 +1,13 @@
 import BookingCard from "./BookingCard";
 import bookings from "../../data/bookingsData";
+import { Link } from "react-router-dom";
 
 const BookingsComponent = () => {
   return (
     <div className="w-[90%] sm:w-[60%] mx-auto sm:mx-4">
       <div className="w-[100%] flex justify-between">
         <span role="button">Upcoming</span>
-        <span role="button">See All</span>
+        <Link to="/bookings"><span role="button">See All</span></Link>
       </div>
       <div className="flex md:flex-row  gap-2 overflow-x-scroll scrollbar-thin">
         {bookings.map((booking) => (
