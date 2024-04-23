dee62e9d87faca06b51e9a459096b184404c514e
Benson Njuguna <njugunab655@gmail.com>
2024-04-23 18:50:55 +0300

    Mock data

---
+++ src/data/messagesData.ts
@@ -0,0 +1,22 @@
+const messages = [
+  {
+    name: "John Doe",
+    email: "njugunaTests@gmail.com ",
+    message: "Hie team thank you for the millimani coverage!",
+    date: "29th Jan 2023",
+  },
+  {
+    name: "John Doe",
+    email: "njugunaTests@gmail.com ",
+    message: "Hie team thank you for the millimani coverage!",
+    date: "29th Jan 2023",
+  },
+  {
+    name: "John Doe",
+    email: "njugunaTests@gmail.com ",
+    message: "Hie team thank you for the millimani coverage!",
+    date: "29th Jan 2023",
+  }
+];
+
+export default messages;
